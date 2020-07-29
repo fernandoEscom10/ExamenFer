3b273ce0d3938d09a282008eec781b3b5f75a472
Fernando <fnegrete9510@gmail.com>
2020-07-28 23:14:28 -0500

    otro

--- app/src/main/java/com/example/examen/MainActivity.kt
+++ app/src/main/java/com/example/examen/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.examen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//creado por fernando
+//creado por fernando 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
