3667b694422445fb0dcc31586974662339549183
Fernando <fnegrete9510@gmail.com>
2020-07-29 17:57:01 -0500

    terminado

--- app/src/main/java/com/example/examen/Data/Prefs.kt
+++ app/src/main/java/com/example/examen/Data/Prefs.kt
@@ -7,6 +7,7 @@ class Prefs (context: Context){
     val PREF_NAME ="sharedpreferences"
     val SHARED_NAME="shared_name"
     val SHARED_PASSWOR="shared_pass"
+    val SHARED_TELEFONO="shared_telefono"
     val prefs: SharedPreferences = context.getSharedPreferences(PREF_NAME,0)
 
     var name: String?
@@ -17,5 +18,8 @@ class Prefs (context: Context){
         get() = prefs.getString(SHARED_PASSWOR, "123456")
         set(value) = prefs.edit().putString(SHARED_PASSWOR, value).apply()
 
+    var telefono: String?
+        get() = prefs.getString(SHARED_TELEFONO, "54545859")
+        set(value) = prefs.edit().putString(SHARED_TELEFONO, value).apply()
 
 }
--- app/src/main/java/com/example/examen/UI/Transaction_activity.kt
+++ app/src/main/java/com/example/examen/UI/Transaction_activity.kt
@@ -2,29 +2,83 @@ package com.example.examen.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
 import com.example.examen.R
+import com.example.examen.SaredApp
 import kotlinx.android.synthetic.main.activity_transaction_activity.*
 
-class Transaction_activity : AppCompatActivity(), View.OnClickListener{
+class Transaction_activity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transaction_activity)
+        tv_nombre.text = (SaredApp.prefs.name)
+        tv_telefono.text = (SaredApp.prefs.telefono)
         ibtn_back.setOnClickListener(this)
+        btn_confirmar.setOnClickListener(this)
+        edt_coment.addTextChangedListener(textWatcher)
+        et_cantidad.addTextChangedListener(textWatcherCant)
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed()
-    }
 
-    override fun onClick(v: View?) {
-        if (v != null) {
-            when(v.id){
-                R.id.ibtn_back->{
-                    onBackPressed()
-                }
-            }
+        override fun onBackPressed() {
+            super.onBackPressed()
         }
 
+        override fun onClick(v: View?) {
+            if (v != null) {
+                when (v.id) {
+                    R.id.ibtn_back -> {
+                        onBackPressed()
+                    }
+                    R.id.btn_confirmar -> {
+                        Toast.makeText(
+                            this,
+                            "Cantidad:  " + et_cantidad.text + "  total:" + et_cantidad.text + "  comentario" + edt_coment.text,
+                            Toast.LENGTH_LONG
+                        ).show()
+                        onBackPressed()
+                    }
+                }
+            }
+
+        }
+
+        val textWatcher  = object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                if (!et_cantidad.text.isNullOrBlank() && !edt_coment.text.isNullOrBlank()) {
+                    btn_confirmar.isEnabled = true
+                }
+
+            }
+
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                }
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                }
+            }
+
+    val textWatcherCant  = object : TextWatcher {
+        override fun afterTextChanged(p0: Editable?) {
+            if (!et_cantidad.text.isNullOrBlank() && !edt_coment.text.isNullOrBlank()) {
+                btn_confirmar.isEnabled = true
+                tv_total.text = p0.toString()
+            }
+
+        }
+
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            if (!et_cantidad.text.isNullOrBlank() && !edt_coment.text.isNullOrBlank()) {
+                btn_confirmar.isEnabled = true
+                tv_total.text = p0.toString()
+            }
+        }
     }
-}
+        }
